2be24a441474682a651db0204b36ef7707685b08
acceberH <qh2130@nyu.edu>
2026-02-05 00:10:25 -0800

    externalize aws sdk in next build

--- web/next.config.ts
+++ web/next.config.ts
@@ -9,6 +9,17 @@ const nextConfig: NextConfig = {
       "@aws-sdk/client-bedrock-runtime",
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push(
+        "@aws-sdk/client-s3",
+        "@aws-sdk/client-sqs",
+        "@aws-sdk/client-bedrock-runtime",
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
