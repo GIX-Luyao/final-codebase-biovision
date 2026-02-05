7addd2fe07088009eabb324302c95c6cce116d04
acceberH <qh2130@nyu.edu>
2026-02-05 02:17:10 -0800

    use local entry and lockfile for amplify function

--- amplify/functions/beaver-api/resource.js
+++ amplify/functions/beaver-api/resource.js
@@ -3,6 +3,6 @@ const { defineFunction } = require("@aws-amplify/backend");
 exports.beaverApi = defineFunction({
   name: "beaver-api",
   runtime: 18,
-  entry: "amplify/functions/beaver-api/index.js",
-  depsLockFilePath: "amplify/functions/beaver-api/package-lock.json",
+  entry: "./index.js",
+  depsLockFilePath: "package-lock.json",
 });
